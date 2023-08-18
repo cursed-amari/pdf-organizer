d615fbec33a809317ce2f0dca6cc72c923668318
Amari <filin1232@gmail.com>
2023-08-18 13:20:17 +0300

    refactoring

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QFileDialog, QMessageBox
 
 from main_class import Ui_MainWindow
+from path_handler import PathHandler
 
 
 logger.add("debug.log", format="{time}, {level}, {message}", level="DEBUG", rotation="2 days", retention="2 days")
@@ -19,9 +20,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.application_func()
+        self.__application_func()
 
-    def application_func(self):
+    def __application_func(self):
         self.setWindowTitle("PDF organizer")
         self.progressBar_convert.setValue(0)
         self.label_complete_convert.hide()
@@ -32,69 +33,27 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton_organizer_path_open.clicked.connect(self.path_organizer)
         self.pushButton_convert.clicked.connect(self.convert_img)
         self.pushButton_organizer.clicked.connect(self.concatenating_pdf)
-        self.checkBox_organizer_midlle_combine.clicked.connect(self.off_checkbox_last)
-        self.checkBox_organizer_last_combine.clicked.connect(self.off_checkbox_middle)
+        self.checkBox_organizer_midlle_combine.clicked.connect(self._off_checkbox_last)
+        self.checkBox_organizer_last_combine.clicked.connect(self._off_checkbox_middle)
 
     """ Converter """
 
     @logger.catch
     def path(self, bool_val=False):
-        self.paths_to_files = QFileDialog.getOpenFileNames(self, filter="(*.png *.jpeg *.jpg *.bmp)")[0]
-        self.hide_progressbar_convert(True)
-        if self.paths_to_files:
-            self.file_path_line(self.paths_to_files)
-
-    @logger.catch
-    def file_path_line(self, path: list):
-        paths_to_files = re.sub(r'\.\w{1,5}$', "", path[0])
-        file_name = re.sub(r'\.\w{1,5}$', "", os.path.basename(path[0]))
-        self.lineEdit_converter_path.setText(paths_to_files.replace(file_name, ""))
-        self.display_img(path)
-
-    @logger.catch
-    def collect_img(self, paths: list) -> list:
-        filter_list_file = []
-        for path in paths:
-            filter_list_file.append(os.path.basename(path))
-
-        return filter_list_file
-
-    @logger.catch
-    def display_img(self, path: list):
-        self.listWidget_convert_object.clear()
-        for i in self.collect_img(path):
-            self.listWidget_convert_object.addItem(i)
-
-    @logger.catch
-    def current_date(self) -> str:
-        return f"{datetime.now().day}.{datetime.now().month}.{datetime.now().year}"
-
-    @logger.catch
-    def correct_path_file(self, file_name: str) -> str:
-        """
-        Поиск имени файла, который не совпадает с существующими
-        :param file_name: 
-        :return: file name
-        """
-        save_name_counter = 0
-        if os.path.exists(f'{os.getcwd()}\\pdf\\{file_name}_{self.current_date()}.pdf'):
-            while True:
-                if os.path.exists(f'{os.getcwd()}\\pdf\\{file_name}_{self.current_date()} ({save_name_counter}).pdf'):
-                    save_name_counter += 1
-                    continue
-                else:
-                    return f'{os.getcwd()}\\pdf\\{file_name}_{self.current_date()} ({save_name_counter}).pdf'
-        else:
-            return f'{os.getcwd()}\\pdf\\{file_name}_{self.current_date()}.pdf'
+        paths_to_files = QFileDialog.getOpenFileNames(self, filter="(*.png *.jpeg *.jpg *.bmp)")[0]
+        self.path_handler = PathHandler(paths_to_files)
+        self._hide_progressbar_convert(False)
+        if self.path_handler:
+            self.__file_path_line()
 
     @logger.catch
     def convert_img(self, bool_val=False):
         self.progressBar_convert.setValue(0)
-        self.hide_progressbar_convert(False)
+        self._hide_progressbar_convert(True)
         try:
-            img_list = self.paths_to_files
+            img_list = self.path_handler.common_combine()
 
-            if self.check_out_folder():
+            if self._check_out_folder():
                 step = 0
                 process = 100/len(img_list)
 
@@ -102,33 +61,52 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                     image_1 = Image.open(entry)
                     im_1 = image_1.convert('RGB')
                     file_name = re.sub(r'\.\w{1,5}$', "", os.path.basename(entry))
-                    im_1.save(self.correct_path_file(file_name))
+                    im_1.save(self._correct_path_file(file_name))
                     step += process
                     self.progressBar_convert.setValue(int(step))
 
                 self.progressBar_convert.setValue(100)
-                self.hide_progressbar_convert(True)
+                self._hide_progressbar_convert(True)
         except AttributeError:
-            error = QMessageBox()
-            error.setWindowTitle('Ошибка')
-            error.setText('Выберите папку')
-            error.setIcon(QMessageBox.Icon.Warning)
-            error.setStandardButtons(QMessageBox.StandardButton.Ok)
-            error.setDefaultButton(QMessageBox.StandardButton.Ok)
-
-            error.exec()
+            self._user_except('Ошибка')
         except ZeroDivisionError:
-            error = QMessageBox()
-            error.setWindowTitle('Ошибка')
-            error.setText('Выберите папку')
-            error.setIcon(QMessageBox.Icon.Warning)
-            error.setStandardButtons(QMessageBox.StandardButton.Ok)
-            error.setDefaultButton(QMessageBox.StandardButton.Ok)
-
-            error.exec()
+            self._user_except('Выберите папку')
 
     @logger.catch
-    def hide_progressbar_convert(self, flag: bool):
+    def __file_path_line(self):
+        self.lineEdit_converter_path.setText(self.path_handler.get_path_to_file())
+        self.__display_img()
+
+    @logger.catch
+    def __display_img(self):
+        self.listWidget_convert_object.clear()
+        for i in self.path_handler.get_list_file_name():
+            self.listWidget_convert_object.addItem(i)
+
+    @logger.catch
+    def _current_date(self) -> str:
+        return f"{datetime.now().day}.{datetime.now().month}.{datetime.now().year}"
+
+    @logger.catch
+    def _correct_path_file(self, file_name: str) -> str:
+        """
+        Поиск имени файла, который не совпадает с существующими
+        :param file_name:
+        :return: file name
+        """
+        save_name_counter = 0
+        if os.path.exists(f'{os.getcwd()}\\pdf\\{file_name}_{self._current_date()}.pdf'):
+            while True:
+                if os.path.exists(f'{os.getcwd()}\\pdf\\{file_name}_{self._current_date()} ({save_name_counter}).pdf'):
+                    save_name_counter += 1
+                    continue
+                else:
+                    return f'{os.getcwd()}\\pdf\\{file_name}_{self._current_date()} ({save_name_counter}).pdf'
+        else:
+            return f'{os.getcwd()}\\pdf\\{file_name}_{self._current_date()}.pdf'
+
+    @logger.catch
+    def _hide_progressbar_convert(self, flag: bool):
         if flag:
             self.progressBar_convert.setValue(0)
             self.progressBar_convert.hide()
@@ -138,139 +116,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.label_complete_convert.hide()
 
     @logger.catch
-    def check_out_folder(self) -> bool:
+    def _check_out_folder(self) -> bool:
         if not os.path.exists(os.getcwd() + "\pdf"):
             os.mkdir(os.getcwd() + "\pdf")
             return True
         else:
             return True
 
-
     """ Organizer """
 
     @logger.catch
-    def off_checkbox_last(self, bool_val=False):
-        self.checkBox_organizer_last_combine.setChecked(False)
-
-    @logger.catch
-    def off_checkbox_middle(self, bool_val=False):
-        self.checkBox_organizer_midlle_combine.setChecked(False)
-
-    @logger.catch
     def path_organizer(self, bool_val=False):
-        self.paths_to_files_organizer = QFileDialog.getOpenFileNames(self, filter="(*.pdf)")[0]
-        self.hide_progressbar_organizer(False)
-        if self.paths_to_files_organizer:
-            self.file_path_line_organizer(self.paths_to_files_organizer)
-
-    @logger.catch
-    def file_path_line_organizer(self, paths: list):
-        paths_to_files = re.sub(r'\.\w{1,5}$', "", paths[0])
-        file_name = re.sub(r'\.\w{1,5}$', "", os.path.basename(paths[0]))
-
-        self.lineEdit_organizer_path.setText(paths_to_files.replace(file_name, ""))
-        self.label_organizer_quantity_pages.setText(str(len(paths)))
-
-    @logger.catch
-    def skip_this_page(self) -> list:
-        if self.lineEdit_organizer_skip_page.text() != "":
-            list_re = re.split(r"(\d+)", self.lineEdit_organizer_skip_page.text())
-            result = []
-            for i in range(1, len(list_re)):
-                if i % 2 != 0:
-                    if list_re[i].isdigit:
-                        result.append(int(list_re[i]))
-            return result
-        else:
-            return []
-
-    @logger.catch
-    def del_skip_page(self, list_no_skip: list) -> list:
-        result_list = list_no_skip
-        if self.skip_this_page():
-            for i in reversed(sorted(self.skip_this_page())):
-                if i != 0:
-                    result_list.pop(i-1)
-            return result_list
-        return result_list
-
-    @logger.catch
-    def pre_sorting(self) -> list:
-        '''
-        Сортировка списка по порядку файлов
-        для стандартных имен window
-        для случайных имён
-        :return: сортированный список
-        '''
-        pre_sorted_list = []
-        sorted_list_files_organizer = []
-
-        for i in self.paths_to_files_organizer:
-            if re.search(r"(.*)\((\d*)\)(.*)", i):
-                pre_sorted_list.append(i)
-        if pre_sorted_list:
-            for i in sorted(self.paths_to_files_organizer, key=lambda x: int(re.search(r"(.*)\((\d*)\)(.*)", x)[2])):
-                sorted_list_files_organizer.append(i)
-        else:
-            return self.paths_to_files_organizer
-
-        return sorted_list_files_organizer
-
-    @logger.catch
-    def sorting_list(self) -> list:
-        '''
-        Сортировка списка
-        last
-        1234 -> 1423
-        middle 
-        1234 -> 1324
-        common
-        1234 -> 1234
-        :return: sort list 
-        '''
-        sorted_list_files_organizer = self.pre_sorting()
-        result_list = []
-
-        if (self.checkBox_organizer_midlle_combine.isChecked() or self.checkBox_organizer_last_combine.isChecked()) and \
-                (len(sorted_list_files_organizer) - len(self.skip_this_page())) % 2 != 0:
-            return self.user_except('Нельзя объединить не чётное количество файлов используя middle или last combine')
-
-        if self.checkBox_organizer_midlle_combine.isChecked():
-            counter_one = 0
-            counter_two = int((len(sorted_list_files_organizer) + 1) / 2)
-
-            for i in range(1, len(sorted_list_files_organizer) + 1):
-                if counter_two == len(sorted_list_files_organizer):
-                    break
-                else:
-                    result_list.append(sorted_list_files_organizer[counter_one])
-                    counter_one += 1
-                    result_list.append(sorted_list_files_organizer[counter_two])
-                    counter_two += 1
-            return self.del_skip_page(result_list)
-        elif self.checkBox_organizer_last_combine.isChecked():
-            counter_plus = 0
-            counter_minus = -1
-
-            for i in range(1, len(sorted_list_files_organizer)+1):
-                if counter_plus and abs(counter_minus)-1 == int((len(sorted_list_files_organizer)+1)/2):
-                    break
-                else:
-                    result_list.append(sorted_list_files_organizer[counter_plus])
-                    counter_plus += 1
-                    result_list.append(sorted_list_files_organizer[counter_minus])
-                    counter_minus -= 1
-            return self.del_skip_page(result_list)
-        else:
-            return self.del_skip_page(sorted_list_files_organizer)
+        paths_to_files_organizer = QFileDialog.getOpenFileNames(self, filter="(*.pdf)")[0]
+        self.path_handler_organizer = PathHandler(paths_to_files_organizer)
+        self._hide_progressbar_organizer(False)
+        if self.path_handler_organizer:
+            self.____file_path_line_organizer()
 
     @logger.catch
     def concatenating_pdf(self, bool_val=False):
         self.progressBar_organizer.setValue(0)
-        self.hide_progressbar_organizer(False)
+        self._hide_progressbar_organizer(False)
         try:
-            if self.paths_to_files_organizer:
-                sort_list = self.sorting_list()
+            if self.path_handler_organizer.get_list_file_name():
+                sort_list = self.__sorting_list()
                 if sort_list:
                     pdf_merger = PdfMerger()
                     step = 0
@@ -286,28 +155,72 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                     except ValueError:
                         pass
             else:
-                error = QMessageBox()
-                error.setWindowTitle('Ошибка')
-                error.setText('Выберите папку')
-                error.setIcon(QMessageBox.Icon.Warning)
-                error.setStandardButtons(QMessageBox.StandardButton.Ok)
-                error.setDefaultButton(QMessageBox.StandardButton.Ok)
-
-                error.exec()
+                self._user_except('Выберите папку')
         except AttributeError:
-            error = QMessageBox()
-            error.setWindowTitle('Ошибка')
-            error.setText('Выберите папку')
-            error.setIcon(QMessageBox.Icon.Warning)
-            error.setStandardButtons(QMessageBox.StandardButton.Ok)
-            error.setDefaultButton(QMessageBox.StandardButton.Ok)
+            self._user_except('Выберите папку')
 
-            error.exec()
-
-        self.hide_progressbar_organizer(True)
+        self._hide_progressbar_organizer(True)
 
     @logger.catch
-    def hide_progressbar_organizer(self, flag: bool):
+    def _off_checkbox_last(self, bool_val=False):
+        self.checkBox_organizer_last_combine.setChecked(False)
+
+    @logger.catch
+    def _off_checkbox_middle(self, bool_val=False):
+        self.checkBox_organizer_midlle_combine.setChecked(False)
+
+    @logger.catch
+    def ____file_path_line_organizer(self):
+        self.lineEdit_organizer_path.setText(self.path_handler_organizer.get_path_to_file())
+        self.label_organizer_quantity_pages.setText(str(len(self.path_handler_organizer.get_list_file_name())))
+
+    @logger.catch
+    def _skip_this_page(self) -> list:
+        if self.lineEdit_organizer_skip_page.text() != "":
+            list_re = re.split(r"(\d+)", self.lineEdit_organizer_skip_page.text())
+            result = []
+            for i in range(1, len(list_re)):
+                if i % 2 != 0:
+                    if list_re[i].isdigit:
+                        result.append(int(list_re[i]))
+            return result
+        else:
+            return []
+
+    @logger.catch
+    def __del_skip_page(self):
+        if self._skip_this_page():
+            self.path_handler_organizer.del_pages(self._skip_this_page())
+
+    @logger.catch
+    def __sorting_list(self) -> list:
+        '''
+        Сортировка списка
+        last
+        1234 -> 1423
+        middle
+        1234 -> 1324
+        common
+        1234 -> 1234
+        :return: sort list
+        '''
+        sorted_list_files_organizer = self.path_handler_organizer.sorting_paths()
+
+        if (self.checkBox_organizer_midlle_combine.isChecked() or self.checkBox_organizer_last_combine.isChecked()) and \
+                (len(sorted_list_files_organizer) - len(self._skip_this_page())) % 2 != 0:
+            return self._user_except('Нельзя объединить не чётное количество файлов используя middle или last combine')
+
+        self.__del_skip_page()
+
+        if self.checkBox_organizer_midlle_combine.isChecked():
+            return self.path_handler_organizer.middle_combine()
+        elif self.checkBox_organizer_last_combine.isChecked():
+            return self.path_handler_organizer.last_combine()
+        else:
+            return sorted_list_files_organizer
+
+    @logger.catch
+    def _hide_progressbar_organizer(self, flag: bool):
         if flag:
             self.progressBar_organizer.setValue(0)
             self.progressBar_organizer.hide()
@@ -317,7 +230,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.label_complete_combine.hide()
 
     @logger.catch
-    def user_except(self, text: str):
+    def _user_except(self, text: str):
         error = QMessageBox()
         error.setWindowTitle('Ошибка')
         error.setText(text)
---
+++ path_handler.py
@@ -0,0 +1,92 @@
+import os
+import re
+
+test = ['C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (1)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (2)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (3)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (4)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (5)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (6)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (7)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (8)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (9)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (10)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (11)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (12)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (13)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (14)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (15)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (16)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (17)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (18)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (19)_16.8.2023.pdf', 'C:/Users/user/PycharmProjects/pdf_organizer/pdf/Рисунок (20)_16.8.2023.pdf']
+
+
+class PathHandler:
+    def __init__(self, paths: list):
+        self.paths = paths
+
+    def get_path_to_file(self) -> str:
+        paths_to_files = re.sub(r'\.\w{1,5}$', "", self.paths[0])
+        file_name = re.sub(r'\.\w{1,5}$', "", os.path.basename(self.paths[0]))
+
+        return paths_to_files.replace(file_name, "")
+
+    def common_combine(self) -> list:
+        return self.paths
+
+    def last_combine(self) -> list:
+        result_list = []
+        counter_plus = 0
+        counter_minus = -1
+
+        for i in range(1, len(self.paths) + 1):
+            if counter_plus and abs(counter_minus) - 1 == int((len(self.paths) + 1) / 2):
+                break
+            else:
+                result_list.append(self.paths[counter_plus])
+                counter_plus += 1
+                result_list.append(self.paths[counter_minus])
+                counter_minus -= 1
+        return result_list
+
+    def middle_combine(self) -> list:
+        result_list = []
+        counter_one = 0
+        counter_two = int((len(self.paths) + 1) / 2)
+
+        for i in range(1, len(self.paths) + 1):
+            if counter_two == len(self.paths):
+                break
+            else:
+                result_list.append(self.paths[counter_one])
+                counter_one += 1
+                result_list.append(self.paths[counter_two])
+                counter_two += 1
+        return result_list
+
+    def get_list_file_name(self) -> list:
+        filter_list_file = []
+        for path in self.paths:
+            filter_list_file.append(re.sub(r'\.\w{1,5}$', "", os.path.basename(path)))
+
+        return filter_list_file
+
+    def del_pages(self, skip_pages: list) -> list:
+        if skip_pages:
+            for i in reversed(sorted(skip_pages)):
+                if i != 0:
+                    self.paths.pop(i - 1)
+            return self.paths
+        return self.paths
+
+    def sorting_paths(self) -> list:
+        pre_sorted_list = []
+        sorted_list_files_organizer = []
+
+        for i in self.paths:
+            if re.match(r"(.*)\((\d*)\)(.*)", i):
+                pre_sorted_list.append(i)
+            else:
+                pre_sorted_list.append(False)
+
+        if False not in pre_sorted_list:
+            for i in sorted(self.paths, key=lambda x: int(re.search(r"(.*)\((\d*)\)(.*)", x)[2])):
+                sorted_list_files_organizer.append(i)
+            return sorted_list_files_organizer
+        else:
+            return self.paths
+
+
+a = PathHandler(test)
+# print(a.get_path_to_file())
+# print(a.common_combine())
+# print(a.last_combine())
+# print(a.middle_combine())
+# a.del_pages([1, 20])
+# print(a.last_combine())
+# for i in a.get_list_file_name():
+#     print(i)
+print(a.sorting_paths())
